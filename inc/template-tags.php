3ca6e2b9c00fab515ddcbef92e7c3b77265a263b
sauravxtha <sauravxtha@gmail.com>
2020-05-09 15:23:28 +0545

    updated template tags

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -37,15 +37,14 @@ if ( ! function_exists( 'postmandu_posted_on' ) ) :
 	 * Prints HTML with meta information for the current post-date/time.
 	 */
 	function postmandu_posted_on() {
-		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
-		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+		$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
+		if ( get_the_time( 'U' ) ) {
 			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
 		}
-
 		$time_string = sprintf(
 			$time_string,
 			esc_attr( get_the_date( DATE_W3C ) ),
-			esc_html( get_the_date() ),
+			esc_html( get_the_date() )
 		);
 
 		$posted_on = sprintf(
@@ -57,6 +56,7 @@ if ( ! function_exists( 'postmandu_posted_on' ) ) :
 		echo '<span class="posted-on">' . $posted_on . '</span>'; // WPCS: XSS OK.
 
 	}
+
 endif;
 
 if ( ! function_exists( 'postmandu_posted_by' ) ) :
