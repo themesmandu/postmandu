5c39c1639e7aa1c5b1c90c88cae7c2b9d1c4ed3e
sauravxtha <sauravxtha@gmail.com>
2020-07-30 15:07:06 +0545

    seearch in menu fixed

--- header.php
+++ header.php
@@ -13,24 +13,24 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="profile" href="https://gmpg.org/xfn/11">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
 
-    <?php wp_head(); ?>
+	<?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
-    <?php wp_body_open(); ?>
-    <div id="page" class="site">
-        <header id="top-header" class="site-header" role="banner">
-            <div class="header-wrap">
-                <?php if ( simple_podcast_get_theme_option( 'skip_to_content_toggle' ) ) : ?>
-                <a class="skip-link" href="#content"><?php esc_html_e( 'To the content', 'simple-podcast' ); ?></a>
-                <?php endif; ?>
+	<?php wp_body_open(); ?>
+	<div id="page" class="site">
+		<header id="top-header" class="site-header" role="banner">
+			<div class="header-wrap">
+				<?php if ( simple_podcast_get_theme_option( 'skip_to_content_toggle' ) ) : ?>
+				<a class="skip-link" href="#content"><?php esc_html_e( 'To the content', 'simple-podcast' ); ?></a>
+				<?php endif; ?>
 
-                <?php
-				if ( get_theme_mod( 'menubar_mode' ) === 'alt' ) {
+				<?php
+				if ( simple_podcast_get_theme_option( 'menubar_mode' ) === 'alt' ) {
 					// alternative navigation bar:
 					// left: logo, main menu - right: search form or something.
 					get_template_part( 'template-parts/navigation/main-navbar', 'alt' );
@@ -43,15 +43,15 @@
 				//header page title.
 				simple_podcast_header_page_title();
 				?>
-                <?php
-			if ( is_front_page() && ! is_home() && get_theme_mod( 'banner_toggle' ) ) {
-				// head banner on the front page if it enabled.
-				get_template_part( 'template-parts/jumbotron' );
-			}
-			?>
+				<?php
+				if ( is_front_page() && ! is_home() && get_theme_mod( 'banner_toggle' ) ) {
+					// head banner on the front page if it enabled.
+					get_template_part( 'template-parts/jumbotron' );
+				}
+				?>
 
-            </div>
+			</div>
 
-        </header><!-- #masthead -->
+		</header><!-- #masthead -->
 
-        <div id="content" class="content-wrap">
+		<div id="content" class="content-wrap">
