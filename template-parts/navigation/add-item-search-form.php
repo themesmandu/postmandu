bc23a2aee264333b52b610b07cd8169159c4b3fb
Rupesh Rai <51738506+rupeshchamling@users.noreply.github.com>
2020-08-29 16:55:05 +0545

    addedlabel in search form

--- template-parts/navigation/add-item-search-form.php
+++ template-parts/navigation/add-item-search-form.php
@@ -15,6 +15,7 @@
 	<?php esc_html_e( 'Search', 'simple-podcast' ); ?>
 </label>
 	<div class="input-group">
+		<label for='search' class='screen-reader-text'><?php _e( 'Search', 'simple-podcast' ); ?></label> 
 		<input class="field form-control" id="s" name="s" type="text" placeholder="<?php esc_attr_e( 'Search &hellip;', 'simple-podcast' ); ?>" value="<?php the_search_query(); ?>">
 		<span class="input-group-append">
 			<input class="submit btn-simple-podcast" id="searchsubmit" name="submit" type="submit" value="<?php esc_attr_e( 'Search', 'simple-podcast' ); ?>">
