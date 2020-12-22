9687596326953d216da00d1009951667987d330d
sauravxtha <sauravxtha@gmail.com>
2020-12-22 12:29:32 +0545

    updated footer

--- footer.php
+++ footer.php
@@ -16,10 +16,10 @@
 
 
 <footer id="footer" role="contentinfo">
-	<?php if (is_active_sidebar('simple-podcast_newsletter')) : ?>
+	<?php if ( is_active_sidebar( 'simple-podcast_newsletter' ) ) : ?>
 		<div class="newsletter-widgets">
 			<div class="container">
-				<?php dynamic_sidebar('simple-podcast_newsletter'); ?>
+				<?php dynamic_sidebar( 'simple-podcast_newsletter' ); ?>
 			</div>
 		</div>
 	<?php endif; ?>
@@ -27,19 +27,19 @@
 	<div class="overlay"></div>
 	<?php
 	$active = array();
-	for ($i = 1; $i <= 4; $i++) {
-		if (is_active_sidebar('footer-' . $i)) {
+	for ( $i = 1; $i <= 4; $i++ ) {
+		if ( is_active_sidebar( 'footer-' . $i ) ) {
 			$active[] = $i;
 		}
 	}
 	?>
-	<?php if (0 !== count($active)) { ?>
+	<?php if ( 0 !== count( $active ) ) { ?>
 		<div class="widget-content-wrap">
 			<div class="container">
 				<div id="footer-widgets" class="row content">
-					<?php foreach ($active as $footer_widget_id) : ?>
+					<?php foreach ( $active as $footer_widget_id ) : ?>
 						<div class="col-lg-3 col-sm-6 column">
-							<?php dynamic_sidebar('footer-' . $footer_widget_id); ?>
+							<?php dynamic_sidebar( 'footer-' . $footer_widget_id ); ?>
 						</div>
 					<?php endforeach; ?>
 				</div><!-- #footer-widgets -->
@@ -53,14 +53,15 @@
 			<?php if ( simple_podcast_get_theme_option( 'footer_copyright' ) ) : ?>
 			<div class="site-info">
 				<?php
+				/* translators: %s: copyright text*/
 				$simple_podcast_footercopyright = sprintf( __( '%s', 'simple-podcast' ), simple_podcast_get_theme_option( 'footer_copyright' ) );
-				echo wp_kses_post($simple_podcast_footercopyright);
+				echo wp_kses_post( $simple_podcast_footercopyright );
 				?>
 			</div><!-- .site-info -->
 			<?php endif; ?>
 
 			<?php
-			if (has_nav_menu('social')) :
+			if ( has_nav_menu( 'social' ) ) :
 				wp_nav_menu(
 					array(
 						'theme_location' => 'social',
@@ -72,7 +73,7 @@
 			?>
 		</div>
 	</div><!-- .footer-copyright -->
-	<button class="up-btn btn-simple-podcast" id="up-btn" title="<?php echo esc_html(__('Go to top', 'simple-podcast')); ?>" style="display: block;"><span class="screen-reader-text">to the top</span>&uarr;</button>
+	<button class="up-btn btn-simple-podcast" id="up-btn" title="<?php echo esc_html( __( 'Go to top', 'simple-podcast' ) ); ?>" style="display: block;"><span class="screen-reader-text">to the top</span>&uarr;</button>
 </footer>
 
 </div><!-- #page -->
